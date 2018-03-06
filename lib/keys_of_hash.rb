ce823f93d38f79311f6dd8164b886f15d08af7ef
Arthur Lurye <arthurlurye@gmail.com>
2018-03-06 12:37:49 -0500

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+require ('pry')
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+  key_args = []
+
+      self.each{|animal, country|
+     if arguments.include?(country)
+        key_args.push(animal)
+      end
+      }
+
+
+return key_args
   end
 end
+
+
+# return an array with every key from the hash whose value matches the value(s) given as an argument.
